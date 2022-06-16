d70150acdf9e395d72fdcf999f4ca8a49803aa82
DylanParenzee <dylanparenzee@hotmail.com>
2022-06-16 17:43:13 +1000

    practiced fetching data from API, saved api to variable, commented out so no api call is made after every refresh

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-let bitcoin = fetch('https://api.coincap.io/v2/assets/bitcoin')
+// const key = 'bd46a268c8e5e6bd7778516a6f3f569c'
+// let cityName = 'auckland'
 
-console.log(bitcoin)
 
-bitcoin.then(()=>{
-    
-})
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`)
+// .then((res) => {
+//     if(res.status !== 200){
+//         throw new Error(res.status)
+//     }
+//     return res.json()
+// })
+// .then((data) => {
+//     console.log(data)
+//     console.log(data.name)
+//     console.log(` Temp is currently : ${data.main.temp}C`)
+//     console.log(`conditions : ${data.weather[0].description}`)
+//     console.log(` wind speed : ${data.wind.speed}`)
+// })
+// .catch((err) => {
+//     console.log(`${err} :  unable to load data`)
+// })
