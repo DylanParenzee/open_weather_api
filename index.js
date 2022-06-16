508b18614cccacd685c6ad7bb86f97363ba396dd
DylanParenzee <dylanparenzee@hotmail.com>
2022-06-16 19:49:14 +1000

    minor changes to api call

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 // let cityName = 'auckland'
 
 
-// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`)
-// .then((res) => {
-//     if(res.status !== 200){
-//         throw new Error(res.status)
-//     }
-//     return res.json()
-// })
-// .then((data) => {
-//     console.log(data)
-//     console.log(data.name)
-//     console.log(` Temp is currently : ${data.main.temp}C`)
-//     console.log(`conditions : ${data.weather[0].description}`)
-//     console.log(` wind speed : ${data.wind.speed}`)
-// })
-// .catch((err) => {
-//     console.log(`${err} :  unable to load data`)
-// })
+// // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`)
+// // .then((res) => {
+// //     if(res.status !== 200){
+// //         throw new Error(res.status)
+// //     }
+// //     return res.json()
+// // })
+// // .then((data) => {
+// //     console.log(data)
+// //     console.log(data.name)
+// //     console.log(` Temp is currently : ${data.main.temp}C`)
+// //     console.log(`conditions : ${data.weather[0].description}`)
+// //     console.log(` wind speed : ${data.wind.speed}`)
+// // })
+// // .catch((err) => {
+// //     console.log(`${err} :  unable to load data`)
+// // })
+
+
